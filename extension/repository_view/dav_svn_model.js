2eddaa1f48f1abde1026e9c02a1d5783502313a6
Masamitsu MURASE <masamitsu.murase@gmail.com>
2012-03-04 19:27:50 +0900

    Add tree view.

--- extension/repository_view/dav_svn_model.js
+++ extension/repository_view/dav_svn_model.js
@@ -7,6 +7,7 @@ var DavSvnResource = function(name, type, info){
     this.m_name = name;
     this.m_state = DavSvnResource.STATE_NOT_LOADED;
     this.m_children = {};
+    this.m_parent = null;
     this.m_info = (info || {});
 };
 DavSvnResource.prototype = {
@@ -28,28 +29,69 @@ DavSvnResource.prototype = {
         this.m_dir_state = state;
     },
 
+    isRoot: function(){
+        return (this.name() === "" && this.isDirectory());
+    },
+    isDirectory: function(){
+        return this.type() == DavSvnResource.TYPE_DIRECTORY;
+    },
+    isFile: function(){
+        return this.type() == DavSvnResource.TYPE_FILE;
+    },
+
     addChild: function(child){
         var name = child.name();
         this.m_children[name] = child;
+        child.setParent(this);
     },
-    children: function(){ return this.m_children; },
+    children: function(){
+        var keys = [];
+        for (var k in this.m_children){
+            keys.push(k);
+        }
+        keys.sort();
+        return keys.map(function(k){ return this.m_children[k]; }, this);
+    },
+    childDirs: function(){
+        return this.children().filter(function(child){ return child.isDirectory(); });
+    },
+    childFiles: function(){
+        return this.children().filter(function(child){ return child.isFile(); });
+    },
     hasChild: function(name){
         return this.m_children[name] != null;
     },
     findChild: function(name){
         return this.m_children[name];
     },
+    parent: function(){ return this.m_parent; },
+    setParent: function(parent){
+        this.m_parent = parent;
+    },
+
+    path: function(){
+        var path = this.name();
+        var resource = this.parent();
+        while(resource && !(resource.isRoot())){
+            path = resource.name() + "/" + path;
+            resource = resource.parent();
+        }
+        return path;
+    },
 
     debugInfo: function(level){
         level = (level || 0);
         var space = "";
         for (var i=0; i<level; i++){
-            space += " ";
+            space += "    ";
         }
         var str = space + this.name() + ": " + (this.type()==DavSvnResource.TYPE_FILE ? "file" : "dir");
+        var keys = [];
         for (var k in this.m_children){
-            str = str + "\n" + this.m_children[k].debugInfo(level + 1);
+            keys.push(k);
         }
+        keys.sort();
+        str += "\n" + keys.map(function(k){ return this.m_children[k].debugInfo(level + 1); }, this).join("\n");
         return str;
     }
 };
@@ -91,6 +133,7 @@ DavSvnModel.prototype = {
             listener.update();
         });
     },
+
     repositoryInfo: function(){
         return {
             uuid: "not implemented yet",
@@ -107,8 +150,8 @@ DavSvnModel.prototype = {
             // add files
             var target = obj.file_list.shift();
             var target_resource = new DavSvnResource(target.path.split("/").pop(),
-                                                     (target.type == "file" ? DavSvnResource.TYPE_DIRECTORY
-                                                      : DavSvnResource.TYPE_FILE));
+                                                     (target.type == "file" ? DavSvnResource.TYPE_FILE
+                                                      : DavSvnResource.TYPE_DIRECTORY));
             obj.file_list.forEach(function(file){
                 target_resource.addChild(new DavSvnResource(file.path.split("/").pop(),
                                                             (file.type == "file" ? DavSvnResource.TYPE_FILE
@@ -123,6 +166,10 @@ DavSvnModel.prototype = {
 
     setResource: function(resource, path){
         var dir = this.m_root_dir = (this.m_root_dir || new DavSvnResource("", DavSvnResource.TYPE_DIRECTORY));
+        if (path === ""){
+            return;
+        }
+
         var paths = path.split("/");
         paths.pop();
         paths.forEach(function(name){
@@ -136,6 +183,25 @@ DavSvnModel.prototype = {
         });
         dir.addChild(resource);
     },
+    resource: function(path){
+        if (path === "" || !path){
+            // this means root.
+            return this.m_root_dir;
+        }
+
+        var paths = path.split("/");
+        var dir = this.m_root_dir;
+        for (var i=0; i<paths.length; i++){
+            var name = paths[i];
+            if (dir.hasChild(name)){
+                dir = dir.findResource(name);
+            }else{
+                return null;
+            }
+        }
+        return dir;
+    },
+
     directoryTree: function(){
        
     }
--- extension/repository_view/repository_view.js
+++ extension/repository_view/repository_view.js
@@ -2,13 +2,78 @@
 (function(){
     var DirectoryView = function(view_elem, model){
         this.m_view_elem = view_elem;
+        this.m_root_div = this.createRootDiv();
+        this.m_view_elem.appendChild(this.m_root_div);
         this.m_model = model;
     };
     DirectoryView.prototype = {
         update: function(){
-            this.m_view_elem.innerHTML = this.m_model.m_root_dir.debugInfo();
+            this.updateTreeView();
+        },
+
+        updateTreeView: function(){
+            var root_div = this.m_root_div;
+
+            // remove current children
+            var children = root_div.childNodes;
+            for (var i=0; i<children.length; i++){
+                root_div.removeChild(children[i]);
+            }
+
+            // append new child
+            var dir = this.m_model.resource("");
+            root_div.appendChild(this.createResourceNode(dir));
+        },
+
+        createRootDiv: function(){
+            var elem = document.createElement("div");
+            elem.className = DirectoryView.CLASS_ROOT_DIV;
+            return elem;
+        },
+
+        createResourceNode: function(dir){
+            var elem = document.createElement("ul");
+
+            // Information of this element
+            var li_info = document.createElement("li");
+            li_info.className = DirectoryView.CLASS_INFO_LI;
+            elem.appendChild(li_info);
+
+            var span_name = document.createElement("span");
+            if (dir.isRoot()){
+                span_name.appendChild(document.createTextNode(this.m_model.repositoryInfo().root_url));
+            }else{
+                span_name.appendChild(document.createTextNode(dir.name()));
+            }
+            li_info.appendChild(span_name);
+            var path = dir.path();
+            var self = this;
+            span_name.addEventListener("click", function(){
+                self.m_model.reloadPath(path);
+            });
+
+            if (dir.isDirectory()){
+                // Information of children
+                var li_children = document.createElement("li");
+                li_children.className = DirectoryView.CLASS_CHILDREN_LI;
+                elem.appendChild(li_children);
+
+                var ul_children = document.createElement("ul");
+                li_children.appendChild(ul_children);
+
+                dir.childDirs().concat(dir.childFiles()).forEach(function(child){
+                    var li = document.createElement("li");
+                    ul_children.appendChild(li);
+                    li.appendChild(this.createResourceNode(child));
+                }, this);
+            }
+
+            return elem;
         }
     };
+    DirectoryView.CLASS_ROOT_DIV = "svn_root";
+    DirectoryView.CLASS_INFO_LI = "svn_info";
+    DirectoryView.CLASS_CHILDREN_LI = "svn_children";
 
 
     var load = function(){
